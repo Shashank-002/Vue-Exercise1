600bc2adfb99702c3848866c5351f672f25c7e99
Shashank <shashank.shrivastav@innotechteam.in>
2024-09-17 15:08:01 +0530

    Calculate Age

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div>
-   <userDetail/>
+    <userDetail />
   </div>
- </template>
- 
- <script>
- import userDetail from './components/userDetail.vue'
- 
- export default {
-   name: 'App',
-   components: {
-     userDetail,
-   }
- }
- </script>
- 
- <style>
- #app {
-   font-family: Avenir, Helvetica, Arial, sans-serif;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale;
-   text-align: center;
-   color: #2c3e50;
-   margin-top: 60px;
- }
- </style>
- 
+</template>
+
+<script>
+import userDetail from './components/userDetail.vue'
+
+export default {
+  name: 'App',
+  components: {
+    userDetail,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/components/userDetail.vue
+++ src/components/userDetail.vue
@@ -4,19 +4,34 @@
         <p><strong>Name: </strong> {{ userName }}</p>
         <p><strong>Address:</strong> {{ address }}</p>
         <p><strong>DOB: </strong>{{ dob }}</p>
+        <button @click="calculateAge">Calculate Age</button>
+        <p v-if="age !== null">{{ age }} years old</p>
     </div>
 </template>
 
 <script>
 export default {
-    name:'userDetail',
-        data()
-        {
-            return{
-          userName: 'Ashish',
-          address: 'Connaught place, New Delhi',
-          dob: '18-05-2000'
+    name: 'userDetail',
+    data() {
+        return {
+            userName: 'Ashish',
+            address: 'Connaught place, New Delhi',
+            dob: '2000-05-18',
+            age: null
+        }
+    },
+    methods: {
+        calculateAge() {
+            const dob = new Date(this.dob);
+            const today = new Date();
+            let age = today.getFullYear() - dob.getFullYear();
+            const MonthDiff = today.getMonth() - dob.getMonth();
+
+            if (MonthDiff < 0 || MonthDiff === 0 && today.getDate() < dob.getDate()) {
+                age--;
             }
+            this.age = age;
         }
     }
+}
 </script>
