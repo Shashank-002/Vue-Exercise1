8108030ab39222f42e0647c80b4114a72b9463d1
Shashank <shashank.shrivastav@innotechteam.in>
2024-09-17 12:57:59 +0530

    ShowUserDetails

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+   <userDetail/>
+  </div>
+ </template>
+ 
+ <script>
+ import userDetail from './components/userDetail.vue'
+ 
+ export default {
+   name: 'App',
+   components: {
+     userDetail,
+   }
+ }
+ </script>
+ 
+ <style>
+ #app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
+ }
+ </style>
+ 
---
+++ src/components/userDetail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>User Information</h1>
+        <p><strong>Name: </strong> {{ userName }}</p>
+        <p><strong>Address:</strong> {{ address }}</p>
+        <p><strong>DOB: </strong>{{ dob }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'userDetail',
+        data()
+        {
+            return{
+          userName: 'Ashish',
+          address: 'Connaught place, New Delhi',
+          dob: '18-05-2000'
+            }
+        }
+    }
+</script>
