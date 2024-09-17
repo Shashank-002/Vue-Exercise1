580e765cfdbe3a3cae4f081f6d9e9ecaba82e854
Shashank <shashank.shrivastav@innotechteam.in>
2024-09-17 16:09:03 +0530

    Validate Age

--- src/components/userDetail.vue
+++ src/components/userDetail.vue
@@ -6,6 +6,9 @@
         <p><strong>DOB: </strong>{{ dob }}</p>
         <button @click="calculateAge">Calculate Age</button>
         <p v-if="age !== null">{{ age }} years old</p>
+        <button @click="validateAge">Validate Age</button>
+        <p v-if="isValidated && age !== null && age < 18">You are under age</p>
+        <p v-else-if="isValidated && age !== null && age >= 18">you are ok to use the website</p>
     </div>
 </template>
 
@@ -17,20 +20,23 @@ export default {
             userName: 'Ashish',
             address: 'Connaught place, New Delhi',
             dob: '2000-05-18',
-            age: null
+            age: null,
+            isValidated: false
         }
     },
     methods: {
         calculateAge() {
             const dob = new Date(this.dob);
             const today = new Date();
-            let age = today.getFullYear() - dob.getFullYear();
-            const MonthDiff = today.getMonth() - dob.getMonth();
-
-            if (MonthDiff < 0 || MonthDiff === 0 && today.getDate() < dob.getDate()) {
-                age--;
-            }
+            const diffInMiliSecond = today - dob;
+            const age = Math.floor(diffInMiliSecond / (1000 * 60 * 60 * 24 * 365.25));
             this.age = age;
+        },
+        validateAge() {
+            if (this.age === null) {
+                this.calculateAge();
+            }
+            this.isValidated= true
         }
     }
 }
